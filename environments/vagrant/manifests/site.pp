7aef321e0a25529dcff24d7770ad245f1500beae
Juan Francisco Sanchez Ramos <juanfrasr@gmail.com>
2015-11-20 15:39:47 +0100

    add mongo connection and creat database

--- environments/vagrant/manifests/site.pp
+++ environments/vagrant/manifests/site.pp
@@ -2,9 +2,16 @@ class { 'nodejs':
    repo_url_suffix     => 'node_5.x',
    manage_package_repo => true,
    nodejs_package_ensure  => 'latest',
-}->
-
-package { 'pm2':
+}->package { 'pm2':
    ensure   => 'present',
    provider => 'npm',
+}
+
+class {'::mongodb::server':
+  auth => true,
+}
+
+mongodb::db { 'testdb':
+  user          => 'user1',
+  password_hash => 'a15fbfca5e3a758be80ceaf42458bcd8',
 }
